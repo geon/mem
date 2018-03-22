fe70afcf72160a6f01e36e28fd1287e344d50c6e
Victor Widell <victor@topmost.se>
2018-03-22 20:43:42 +0100

    Fixed bundle path.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
 var path = require("path");
 
 module.exports = {
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  entry: "./src/main.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/"
-  },
-  module: {
-    rules: [
-      { test: /\.ts?$/, loader: "ts-loader" }
-    ]
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    host: "0.0.0.0",
-    contentBase: path.join(__dirname, "src"),
-    stats: {
-      assets: false,
-      hash: false,
-      chunks: false,
-      errors: true,
-      errorDetails: true,
-    },
-    overlay: true
-  }
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	entry: "./src/main.ts",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/",
+	},
+	module: {
+		rules: [{ test: /\.ts?$/, loader: "ts-loader" }],
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		host: "0.0.0.0",
+		contentBase: path.join(__dirname, "src"),
+		stats: {
+			assets: false,
+			hash: false,
+			chunks: false,
+			errors: true,
+			errorDetails: true,
+		},
+		overlay: true,
+	},
 };
