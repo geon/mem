ffb112cbeb16a639307e2e0ef19ad344a07f52ae
Victor Widell <victor@topmost.se>
2018-03-31 09:46:23 +0200

    Added half sphere with rim mesh generator.

--- src/functions.ts
+++ src/functions.ts
@@ -54,8 +54,6 @@ export function makeTesselatedCubeMesh(tesselation: number): TriangleSoup {
 		normal: Coord3,
 	): TriangleSoup {
 		const vertices: Array<Array<Vertex>> = [];
-		const triangles: MutableTriangleSoup = [];
-
 		for (let v = 0; v <= tesselation; ++v) {
 			const vFactor = v / tesselation;
 
@@ -80,6 +78,7 @@ export function makeTesselatedCubeMesh(tesselation: number): TriangleSoup {
 			}
 		}
 
+		const triangles: MutableTriangleSoup = [];
 		for (let v = 0; v < tesselation; ++v) {
 			for (let u = 0; u < tesselation; ++u) {
 				triangles.push([
@@ -242,4 +241,149 @@ export function meshToWebglArrays(mesh: TriangleSoup): twgl.Arrays {
 		},
 		indices: { numComponents: 3, data: indices },
 	};
+}
+
+export function makeCloakMesh(
+	innerRadius: number,
+	thickness: number,
+	uTesselation: number,
+): TriangleSoup {
+	const vTesselation = uTesselation * 4;
+
+	const posY = new Coord3({ x: 0, y: 1, z: 0 });
+	const negX = new Coord3({ x: -1, y: 0, z: 0 });
+	const posZ = new Coord3({ x: 0, y: 0, z: 1 });
+	const startingPoint = negX.scaled(innerRadius + thickness);
+
+	const triangles: MutableTriangleSoup = [];
+
+	const sphereVertices: Array<Array<Vertex>> = [];
+
+	// Half sphere - cap.
+	{
+		for (let v = 0; v <= vTesselation; ++v) {
+			const vFactor = v / vTesselation;
+
+			sphereVertices[v] = [];
+			for (let u = 0; u <= uTesselation; ++u) {
+				const uFactor = u / uTesselation;
+
+				const position = axisRotate(
+					negX,
+					axisRotate(posY, startingPoint, uFactor * 90),
+					vFactor * 360,
+				);
+
+				const normal = position.normalized();
+
+				sphereVertices[v][u] = {
+					position,
+					normal,
+					texCoord: normal,
+				};
+			}
+		}
+		for (let v = 0; v < vTesselation; ++v) {
+			for (let u = 0; u < uTesselation; ++u) {
+				triangles.push([
+					sphereVertices[v + 0][u + 0],
+					sphereVertices[v + 0][u + 1],
+					sphereVertices[v + 1][u + 0],
+				]);
+				triangles.push([
+					sphereVertices[v + 1][u + 0],
+					sphereVertices[v + 0][u + 1],
+					sphereVertices[v + 1][u + 1],
+				]);
+			}
+		}
+	}
+
+	// Cap.
+	{
+		const startingNormal = startingPoint.normalized();
+		const startingVertex = {
+			position: startingPoint,
+			normal: startingNormal,
+			texCoord: startingNormal,
+		};
+		for (let i = 0; i < vTesselation; ++i) {
+			triangles.push([
+				sphereVertices[i][0],
+				sphereVertices[i + 1][0],
+				startingVertex,
+			]);
+		}
+	}
+
+	// Rim
+	{
+		const rimRadius = thickness / 2;
+		const rimCenter = posZ.scaled(innerRadius + rimRadius);
+
+		const rimVertices: Array<Array<Vertex>> = [];
+		const rimTesselation = uTesselation;
+		for (let v = 0; v <= vTesselation; ++v) {
+			const vFactor = v / vTesselation;
+
+			rimVertices[v] = [];
+			for (let u = 0; u <= rimTesselation; ++u) {
+				const uFactor = u / rimTesselation;
+
+				const position = axisRotate(
+					negX,
+					Coord3.add(
+						axisRotate(posY, posZ.scaled(rimRadius), uFactor * 180),
+						rimCenter,
+					),
+					vFactor * 360,
+				);
+
+				const normal = axisRotate(
+					negX,
+					axisRotate(posY, posZ.scaled(rimRadius), uFactor * 180),
+					vFactor * 360,
+				);
+
+				rimVertices[v][u] = {
+					position,
+					normal,
+					texCoord: normal,
+				};
+			}
+		}
+		for (let v = 0; v < vTesselation; ++v) {
+			for (let u = 0; u < rimTesselation; ++u) {
+				triangles.push([
+					rimVertices[v + 0][u + 0],
+					rimVertices[v + 0][u + 1],
+					rimVertices[v + 1][u + 0],
+				]);
+				triangles.push([
+					rimVertices[v + 1][u + 0],
+					rimVertices[v + 0][u + 1],
+					rimVertices[v + 1][u + 1],
+				]);
+			}
+		}
+	}
+
+	return triangles;
+}
+
+function axisRotate(axis: Coord3, coord: Coord3, angleInDeg: number) {
+	return twglVec3ToCoord3(
+		twgl.m4.transformPoint(
+			twgl.m4.axisRotate(
+				twgl.m4.identity(),
+				coord3ToTwglVec3(axis),
+				angleInDeg / 360 * 2 * Math.PI,
+			),
+			coord3ToTwglVec3(coord),
+		),
+	);
+}
+
+function twglVec3ToCoord3(coord: twgl.Vec3): Coord3 {
+	return new Coord3({ x: coord[0], y: coord[1], z: coord[2] });
 }
